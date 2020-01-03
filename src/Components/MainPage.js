270ae8704b15a07a81b3fefd912a43351344e30f
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-01-02 22:15:40 -0500

    Organise files for project.

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,1 +1,18 @@
-import React    from 'react'
+import React   from 'react';
+import ListBooks from "./ListBooks";
+
+
+function MainPage ({books}) {
+    return (
+        <div className="app">
+                
+              
+              {
+                books && <ListBooks books={books}/>
+              } 
+         
+        </div>
+    )
+}
+
+export default MainPage;
